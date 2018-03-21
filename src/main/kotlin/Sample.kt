e67029eca4b3527f90feff1f310e3533bd17195c
AdrianRaFo <adrianramirezfornell@gmail.com>
2018-03-21 16:50:31 +0100

    Minor improvement

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -9,16 +9,18 @@ object Sample {
     println("1".safeToInt())
   }
 
+  val optionKleisli = Kleisli { str: String ->
+    if (str.toCharArray().all { it.isDigit() }) Some(str.toInt()) else None
+  }
+
   fun String.safeToInt(): Either<String, Int> {
-    val optionKleisli = Kleisli { str: String ->
-      if (str.toCharArray().all { it.isDigit() }) Some(str.toInt()) else None
-    }
 
     val eitherFromOptionKleisli = Kleisli { str: String ->
-        optionKleisli.run(str).ev().fold(
-            { Left(str) },
-            { number -> Right(number) })
+      optionKleisli.run(str).ev().fold(
+          { Left(str) },
+          { number -> Right(number) })
     }
+
     return eitherFromOptionKleisli.run(this).ev()
   }
 }
