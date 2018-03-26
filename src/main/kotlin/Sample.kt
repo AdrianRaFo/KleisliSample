671f62262e0b53289d89864d0ad9c1b6e264b71c
AdrianRaFo <adrianramirezfornell@gmail.com>
2018-03-26 20:10:46 +0200

    More examples

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -1,7 +1,10 @@
+
 import arrow.core.*
 import arrow.data.Kleisli
 import arrow.data.ev
+import arrow.data.monad
 import arrow.syntax.functor.map
+import arrow.typeclasses.binding
 
 object Sample {
 
@@ -17,7 +20,26 @@ object Sample {
     println(optionIntKleisli.andThen(optionFromOptionKleisli, Option.monad()).ev().run("1"))
     println(optionIntKleisli.andThen({ number: Int -> Some(number + 1) }, Option.monad()).ev().run("1"))
     println(optionIntKleisli.andThen(Some(0), Option.monad()).ev().run("1"))
+    println(configKleisli.run(Config(1,2.0)))
+    println(askKleisli.run(Config(1,2.0)))
   }
+
+  val k1 = Kleisli { intNumber: Int -> Some(intNumber.toString()) }
+  val k2 = Kleisli { doubleNumber: Double -> Some(doubleNumber.toString()) }
+
+  data class Config(val n: Int, val d: Double)
+
+  val configKleisli : Kleisli<OptionHK, Config, String> =
+      Kleisli.monad<OptionHK, Config>().binding {
+        val a = k1.local<Config> { it.n }.bind()
+        val b = k2.local<Config> { it.d }.bind()
+        a + b
+      }.ev()
+
+  val askKleisli = Kleisli.monad<OptionHK, Config>().binding {
+    val (n, d) = Kleisli.ask<OptionHK, Config>().bind()
+    n + d
+  }.ev()
 
   val optionIntKleisli = Kleisli { str: String ->
     if (str.toCharArray().all { it.isDigit() }) Some(str.toInt()) else None
