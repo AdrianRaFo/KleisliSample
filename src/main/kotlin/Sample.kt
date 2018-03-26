f815996ef1c46e66480be2f04c37f8a4e2a611fa
AdrianRaFo <adrianramirezfornell@gmail.com>
2018-03-26 11:43:13 +0200

    Complete example

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -1,5 +1,7 @@
 import arrow.core.*
 import arrow.data.Kleisli
+import arrow.data.ev
+import arrow.syntax.functor.map
 
 object Sample {
 
@@ -7,17 +9,41 @@ object Sample {
   fun main(args: Array<String>) {
     println("a".safeToInt())
     println("1".safeToInt())
+    println(optionIntKleisli.run("1").ev())
+    println(optionIntKleisli.local { optStr: Option<String> -> optStr.getOrElse { "0" } }.run(None))
+    println(optionIntKleisli.map { output -> output + 1 }.ev().run("1"))
+    println(optionIntKleisli.ap(optionIntDoubleKleisli, Option.applicative()).ev().run("1"))
+    println(optionIntKleisli.flatMap({ optionDoubleKleisli }, Option.monad()).ev().run("1"))
+    println(optionIntKleisli.andThen(optionFromOptionKleisli, Option.monad()).ev().run("1"))
+    println(optionIntKleisli.andThen({ number: Int -> Some(number + 1) }, Option.monad()).ev().run("1"))
+    println(optionIntKleisli.andThen(Some(0), Option.monad()).ev().run("1"))
   }
 
-  val optionKleisli = Kleisli { str: String ->
+  val optionIntKleisli = Kleisli { str: String ->
     if (str.toCharArray().all { it.isDigit() }) Some(str.toInt()) else None
   }
 
+  val intToDouble = { number: Int -> number.toDouble() }
+
+  val optionIntDoubleKleisli = Kleisli { str: String ->
+    if (str.toCharArray().all { it.isDigit() }) Some(intToDouble) else None
+  }
+
+  val optionDoubleKleisli = Kleisli { str: String ->
+    if (str.toCharArray().all { it.isDigit() }) Some(str.toDouble()) else None
+  }
+
+  val optionFromOptionKleisli = Kleisli { number: Int ->
+    Some(number + 1)
+  }
+
+
   val eitherFromOptionKleisli = Kleisli { str: String ->
-    optionKleisli.run(str).ev().fold(
+    optionIntKleisli.run(str).ev().fold(
         { Left(str) },
         { number -> Right(number) })
   }
+
 
   fun String.safeToInt(): Either<String, Int> {
     return eitherFromOptionKleisli.run(this).ev()
