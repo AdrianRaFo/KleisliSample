1b47d9a66a3fe8f762c348946bcc3e5a0ba1c1a6
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2018-04-16 11:22:23 +0200

    Update to Arrow 0.7.1

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -1,9 +1,8 @@
 
 import arrow.core.*
 import arrow.data.Kleisli
-import arrow.data.ev
+import arrow.data.fix
 import arrow.data.monad
-import arrow.syntax.functor.map
 import arrow.typeclasses.binding
 
 object Sample {
@@ -12,16 +11,16 @@ object Sample {
   fun main(args: Array<String>) {
     println("a".safeToInt())
     println("1".safeToInt())
-    println(optionIntKleisli.run("1").ev())
+    println(optionIntKleisli.run("1").fix())
     println(optionIntKleisli.local { optStr: Option<String> -> optStr.getOrElse { "0" } }.run(None))
-    println(optionIntKleisli.map { output -> output + 1 }.ev().run("1"))
-    println(optionIntKleisli.ap(optionIntDoubleKleisli, Option.applicative()).ev().run("1"))
-    println(optionIntKleisli.flatMap({ optionDoubleKleisli }, Option.monad()).ev().run("1"))
-    println(optionIntKleisli.andThen(optionFromOptionKleisli, Option.monad()).ev().run("1"))
-    println(optionIntKleisli.andThen({ number: Int -> Some(number + 1) }, Option.monad()).ev().run("1"))
-    println(optionIntKleisli.andThen(Some(0), Option.monad()).ev().run("1"))
-    println(configKleisli.run(Config(1,2.0)))
-    println(askKleisli.run(Config(1,2.0)))
+    println(optionIntKleisli.map (Option.functor(), {output:Int -> output + 1 }).fix().run("1"))
+    println(optionIntKleisli.ap(Option.applicative(), optionIntDoubleKleisli).fix().run("1"))
+    println(optionIntKleisli.flatMap(Option.monad(), { optionDoubleKleisli }).fix().run("1"))
+    println(optionIntKleisli.andThen(Option.monad(), optionFromOptionKleisli).fix().run("1"))
+    println(optionIntKleisli.andThen(Option.monad(), { number: Int -> Some(number + 1) }).fix().run("1"))
+    println(optionIntKleisli.andThen(Option.monad(), Some(0)).fix().run("1"))
+    println(configKleisli.run(Config(1, 2.0)))
+    println(askKleisli.run(Config(1, 2.0)))
   }
 
   val k1 = Kleisli { intNumber: Int -> Some(intNumber.toString()) }
@@ -29,17 +28,17 @@ object Sample {
 
   data class Config(val n: Int, val d: Double)
 
-  val configKleisli : Kleisli<OptionHK, Config, String> =
-      Kleisli.monad<OptionHK, Config>().binding {
+  val configKleisli: Kleisli<ForOption, Config, String> =
+      Kleisli.monad<ForOption, Config>(Option.monad()).binding {
         val a = k1.local<Config> { it.n }.bind()
         val b = k2.local<Config> { it.d }.bind()
         a + b
-      }.ev()
+      }.fix()
 
-  val askKleisli = Kleisli.monad<OptionHK, Config>().binding {
-    val (n, d) = Kleisli.ask<OptionHK, Config>().bind()
+  val askKleisli = Kleisli.monad<ForOption, Config>(Option.monad()).binding {
+    val (n, d) = Kleisli.ask<ForOption, Config>(Option.monad()).bind()
     n + d
-  }.ev()
+  }.fix()
 
   val optionIntKleisli = Kleisli { str: String ->
     if (str.toCharArray().all { it.isDigit() }) Some(str.toInt()) else None
@@ -61,13 +60,13 @@ object Sample {
 
 
   val eitherFromOptionKleisli = Kleisli { str: String ->
-    optionIntKleisli.run(str).ev().fold(
+    optionIntKleisli.run(str).fix().fold(
         { Left(str) },
         { number -> Right(number) })
   }
 
 
   fun String.safeToInt(): Either<String, Int> {
-    return eitherFromOptionKleisli.run(this).ev()
+    return eitherFromOptionKleisli.run(this).fix()
   }
 }
